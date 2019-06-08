0733b217c372c3df211a94f431c487ce997c82bd
Claire Muwanguzi <31630227+Claire56@users.noreply.github.com>
2019-06-08 13:43:50 -0700

    Update deleteNodeLL.py

--- deleteNodeLL.py
+++ deleteNodeLL.py
@@ -104,6 +104,26 @@ class LinkedList:
             print temp.data, 
             temp = temp.next
   
+## Find if a ll is cyclic, makes a circle, the last node points to the first node
+def cycle_check(node):
+
+    # Begin both markers at the first node
+    marker1 = node
+    marker2 = node
+
+    # Go until end of list
+    while marker2 != None and marker2.nextnode != None:
+        
+        # Note
+        marker1 = marker1.nextnode
+        marker2 = marker2.nextnode.nextnode
+
+        # Check if the markers have matched
+        if marker2 == marker1:
+            return True
+
+    # Case where marker ahead reaches the end of the list
+    return False
 	
 	
 
